7bd8eee9213b542ffff5ca7cf6268c0899acecab
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-06-06 15:18:47 -0500

    Finished lab 1

--- maxarray.cpp
+++ maxarray.cpp
@@ -1,40 +1,39 @@
 /*
-Ch. 2,  Programming Problems 2, pg. 94
-
-Name the program maxarray.cpp. Make sure the following requirements are met. 
-
-    - Program must compile and run.
-    - maxArray function must be a recursive template function.
-    - Add a main function to test the maxArray function
-      so that you have a complete program.
-    - Test the maxArray function on two arrays of different types.
-
-Remember to upload all files before submitting.
-
-      maxarray.cpp
+  Lab 1
+  Author:   Lee Reese
+  Due:      Friday 6/8 
+  Filename: maxarray.cpp
 */
 
 #include <iostream>
 #include <string>
+#include <algorithm> // std::max
 
 template <class ItemType>
-ItemType maxArray(ItemType array[]);
-
+ItemType maxArray(ItemType[], int, int);
 
 int main()
 {
   double x[] = {1.1, 2.2, 7.7, 3.3, 4.4, 5.5};
-  std::cout << maxArray(x) << std::endl;
+  std::cout << maxArray(x, 0, 6) << std::endl;
 
   int num[] = {5, 1, 2, 3, 4};
-  std::cout << maxArray(num) << std::endl;
+  std::cout << maxArray(num, 0, 2) << std::endl;
   
   std::string s[] = {"aa", "bb", "cc"};
-  std::cout << maxArray(s) << std::endl; 
-}
+  std::cout << maxArray(s, 0, 2) << std::endl; 
+} // end main
 
+// Returns the maximum value in the range [first, last] on the array
 template <class ItemType>
-ItemType maxArray(ItemType array[]) {
-  return array[0];
-}
+ItemType maxArray(ItemType array[], int first, int last) {
+  // Base Case
+  if (first == last)                                   
+    return array[first];
+  
+  // Inductive Case
+  else
+    return std::max(maxArray(array, first           , (first+last)/2),
+                    maxArray(array, (first+last)/2+1, last          ));
+} // end maxArray
 
