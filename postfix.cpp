4faf1100eaa9a6dce7d14572de172e9325cff982
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-07-02 14:22:55 -0500

    done with lab 4

--- postfix.cpp
+++ postfix.cpp
@@ -19,39 +19,51 @@ Remember to upload all files before submitting.
 #include <iostream>
 #include <string>
 #include <stack>
-#include <stdlib.h>
 
 int evaluate(std::stack<char>&);
 
 int main() {
-  // Get expression
-  std::cout << "Expression to evaluate: ";
-  std::string expr;
-  getline(std::cin, expr);
+  std::cout << "Postifx expression evaluator.\nLeave Expression empty to quit."
+            << std::endl;
+  bool done = false;
+  while (!done) {
+    // Get expression
+    std::cout << "Expression: ";
+    std::string expr;
+    getline(std::cin, expr);
+    if (expr.empty())
+      done = true;
 
-  // Fill stack
-  std::stack<char> post;
-  for (char& x : expr)
-    post.push(x);
-  
-  std::cout << "Result: " << evaluate(post) << std::endl;
+    // Fill stack
+    std::stack<char> post;
+    for (char& x : expr)
+      post.push(x);
 
-  
+    // Output result
+    std::cout << "Result: " << evaluate(post) << std::endl;
+  } //end while
 
-  
   return 0;
 } // end main
 
 int evaluate(std::stack<char>& post) {
+  // Base case: enpty stack
+  if (post.empty())
+    return 0;
+  // Base case: top is an integer
   char x = post.top(); post.pop();
-  if (47 < x && x < 58) // if x is an integer
+  if (47 < x && x < 58)
     return x - 48;
+
+  // Inductive case(s): top is an operator
   if (x == '+')
     return evaluate(post) + evaluate(post);
-  else if (x == '-')
+  if (x == '-')
     return evaluate(post) - evaluate(post);
-  else if (x == '*')
+  if (x == '*')
     return evaluate(post) * evaluate(post);
-  else if (x == '/')
+  if (x == '/')
     return evaluate(post) / evaluate(post);
+  else
+    return 0;
 }
