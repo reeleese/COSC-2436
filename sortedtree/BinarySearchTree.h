7d63219d9b85c0d22d4f0fa0a6dbd6f55e0252c7
reeleese <reeleese@github.com>
2018-08-01 19:03:23 -0500

    i hate frank m carrano

--- sortedtree/BinarySearchTree.h
+++ sortedtree/BinarySearchTree.h
@@ -21,19 +21,22 @@ class BinarySearchTree : public BinaryTreeInterface<ItemType> {
  // Constructor and Destructor
  //------------------------------------------------------------
  BinarySearchTree() {
-   
+   root = nullptr;
  }
 
- BinarySearchTree(const ItemType& rootItem) {
-   root(std::make_shared<BinaryNode<ItemType>>(rootItem, nullptr, nullptr));
- }
+ BinarySearchTree(const ItemType& rootItem) 
+   :root(std::make_shared<BinaryNode<ItemType>>(rootItem, nullptr, nullptr))
+ {}
  
  BinarySearchTree(const ItemType& rootItem,
 		const std::shared_ptr<BinarySearchTree<ItemType>> leftTreePtr,
-		const std::shared_ptr<BinarySearchTree<ItemType>> rightTreePtr) {
-   
- }
+		const std::shared_ptr<BinarySearchTree<ItemType>> rightTreePtr)
+   :root(std::make_shared<BinaryNode<ItemType>>(rootItem,
+                                                copyTree(leftTreePtr->root),
+                                                copyTree(rightTreePtr->root)))
+   {}
  BinarySearchTree(const BinarySearchTree<ItemType>& tree);
+
  virtual ~BinarySearchTree() {
    root = nullptr;
  }
@@ -41,6 +44,7 @@ class BinarySearchTree : public BinaryTreeInterface<ItemType> {
  //------------------------------------------------------------
  // Public Methods
  //------------------------------------------------------------
+  
  bool isEmpty() const {
    return root == nullptr;
  }
@@ -68,10 +72,16 @@ class BinarySearchTree : public BinaryTreeInterface<ItemType> {
  }
  
  bool add(const ItemType& newData) {
-   return false;
+   shared_ptr_BN newNode = std::make_shared<BinaryNode<ItemType>>(newData);
+   root = placeNode(root, newNode);
+
+   return true;
  }
+ 
  bool remove(const ItemType& data) {
-   return false;
+   bool success = false;
+   root = removeValue(root, data, success);
+   return success;
  }
 
  void clear() {
@@ -94,7 +104,7 @@ class BinarySearchTree : public BinaryTreeInterface<ItemType> {
  }
 
  void inorderTraverse(void visit(ItemType&)) const {
-
+   inorder(visit, root);
  }
  
  void postorderTraverse(void visit(ItemType&)) const {
@@ -109,6 +119,7 @@ class BinarySearchTree : public BinaryTreeInterface<ItemType> {
  //------------------------------------------------------------
  // Protected Methods
  //------------------------------------------------------------
+ protected:
  int getHeightHelper(shared_ptr_BN subTree) const {
    if (subTree == nullptr)
      return 0;
@@ -124,6 +135,111 @@ class BinarySearchTree : public BinaryTreeInterface<ItemType> {
       return 1 + getNumberOfNodesHelper(subTree->getLeftChildPtr()) 
                + getNumberOfNodesHelper(subTree->getRightChildPtr());
  }
+
+ shared_ptr_BN placeNode(shared_ptr_BN subTree, shared_ptr_BN newNode) {
+   if (subTree == nullptr)
+     return newNode;
+   
+   if (subTree->getItem() > newNode->getItem()) {
+     shared_ptr_BN temp = placeNode(subTree->getLeftChildPtr(), newNode);
+     subTree->setLeftChildPtr(temp);
+   }
+
+   else {
+     shared_ptr_BN temp = placeNode(subTree->getRightChildPtr(), newNode);
+     subTree->setRightChildPtr(temp);
+   }
+
+   return subTree;
+ }
+
+ shared_ptr_BN findNode(shared_ptr_BN treePtr, const ItemType& target, bool& success) const {
+   if (treePtr == nullptr) // not found here
+     return treePtr;
+   
+   if (treePtr->getItem() == target) {
+     success = true;
+     return treePtr;
+   }
+   
+   else {
+     shared_ptr_BN targetNodePtr = findNode(treePtr->getLeftChildPtr(), target, success);
+     if (!success) {
+       targetNodePtr = findNode(treePtr->getRightChildPtr(), target, success);
+     }
+      
+     return targetNodePtr;
+   }  // end if 
+ }  // end findNode
+
+ shared_ptr_BN removeValue(shared_ptr_BN subTree, const ItemType& target, bool& success) {
+   if (subTree == nullptr)
+     success = false;
+
+   else if (subTree->getItem() == target) {
+     subTree = removeNode(subTree);
+     success = true;
+   }
+
+   else if (subTree->getItem() > target) {
+     shared_ptr_BN temp = removeValue(subTree->getRightChildPtr(), target, success);
+     subTree->setLeftChildPtr(temp);
+   }
+
+   else {
+     shared_ptr_BN temp = removeValue(subTree->getLeftChildPtr(), target, success);
+     subTree->setRightChildPtr(temp);
+   }
+
+   return subTree;
+ }
+
+ shared_ptr_BN removeNode(shared_ptr_BN node) {
+   if (node->isLeaf()) {
+     return node;
+   }
+
+   else if ((node->getLeftChildPtr() == nullptr) xor (node->getRightChildPtr() == nullptr)) {
+     shared_ptr_BN nodeToConnect;
+     if (node->getLeftChildPtr() != nullptr)
+       nodeToConnect = node->getLeftChildPtr();
+     else
+       nodeToConnect = node->getRightChildPtr();
+     return nodeToConnect;
+   }
+
+   else {
+     shared_ptr_BN temp = removeLeftmostNode(node->getRightChildPtr(), 0);
+     node->setRightChildPtr(temp);
+     node->setItem(0);
+     return node;
+   }
+ }
+
+ shared_ptr_BN removeLeftmostNode(shared_ptr_BN node, ItemType inorderSuccessor) {
+   if (node->getLeftChildPtr() == nullptr) {
+     inorderSuccessor = node->getItem();
+     return removeNode(node);
+   }
+
+   else {
+     shared_ptr_BN temp = removeLeftmostNode(node->getLeftChildPtr(), inorderSuccessor);
+     node->setLeftChildPtr(temp);
+     return node;
+   }
+ }
+
+void inorder(void visit(ItemType&), std::shared_ptr<BinaryNode<ItemType>> treePtr) const
+{
+   if (treePtr != nullptr)
+   {
+      inorder(visit, treePtr->getLeftChildPtr());
+      ItemType theItem = treePtr->getItem();
+      visit(theItem);
+      inorder(visit, treePtr->getRightChildPtr());
+   }  // end if
+}  // end inorder
+
 
  
 };
--- sortedtree/sortedtree.cpp
+++ sortedtree/sortedtree.cpp
@@ -1,32 +1,32 @@
 /*
-Ch. 16, Programming Problems 2, pg. 513
-
-Name the program sortedtree.cpp. Make sure the following requirements are met. 
-
-    - Program must compile and run.
-    - Driver program should:
-        -- insert 21 random numbers (1-100) on the tree
-        -- remove the last number inserted
-        -- and display the tree
-    - To display just use in order traversal
-    - No user input for driver program.
-    - Hint: Inherit from binarytree (binarytree.zip) as a starting point.
-    - Use the BST ADT BinarySearchTree.h 
-
-Remember to upload all files before submitting.
-
-    sortedtree.cpp
+author: lee reese
 */
 
 #include "BinarySearchTree.h"
 
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+void display(int thing) {
+  std::cout << thing << ' ';
+}
 
 int main() {
-  BinarySearchTree<int> test = BinarySearchTree<int>();
-  std::cout << test.isEmpty() << std::endl;
 
-  BinarySearchTree<int> test1 = BinarySearchTree<int>(4);
-  std::cout << test.isEmpty() << std::endl;
+  srand(time(NULL));
+  int rando = 0;
+  BinarySearchTree<int> tree = BinarySearchTree<int>();
+  for (int i = 0; i < 25; i++) {
+    rando = rand() % 100 +1;
+    tree.add(rando);
+  }
+
+  std::cout << "The list with 21 random integers:\n";
+  tree.inorderTraverse(&display(int));
+
+  tree.remove(rando);
+  std::cout << "The list without " << rando << ":\n";
+  
   return 0;
 }
